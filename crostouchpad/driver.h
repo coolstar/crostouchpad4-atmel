4f4440e6ca148282505876c4ad7a6b9d9854da76
CoolStar <coolstarorganization@gmail.com>
2023-10-01 14:07:06 -0700

    Add support for s0ix

--- crostouchpad/driver.h
+++ crostouchpad/driver.h
@@ -204,6 +204,8 @@ typedef struct _ATMELTP_CONTEXT
 
 	WDFQUEUE ReportQueue;
 
+	WDFQUEUE IdleQueue;
+
 	BYTE DeviceMode;
 
 	SPB_CONTEXT I2CContext;
@@ -287,6 +289,20 @@ typedef struct _ATMELTP_CONTEXT
 WDF_DECLARE_CONTEXT_TYPE_WITH_NAME(ATMELTP_CONTEXT, GetDeviceContext)
 
 //
+// Power Idle Workitem context
+// 
+typedef struct _IDLE_WORKITEM_CONTEXT
+{
+	// Handle to a WDF device object
+	WDFDEVICE FxDevice;
+
+	// Handle to a WDF request object
+	WDFREQUEST FxRequest;
+
+} IDLE_WORKITEM_CONTEXT, * PIDLE_WORKITEM_CONTEXT;
+WDF_DECLARE_CONTEXT_TYPE_WITH_NAME(IDLE_WORKITEM_CONTEXT, GetIdleWorkItemContext)
+
+//
 // Function definitions
 //
 
@@ -359,6 +375,11 @@ OUT BOOLEAN* CompleteRequest
 PCHAR
 DbgHidInternalIoctlString(
 IN ULONG        IoControlCode
+);
+
+VOID
+AtmelTPCompleteIdleIrp(
+	IN PATMELTP_CONTEXT FxDeviceContext
 );
 
 //
